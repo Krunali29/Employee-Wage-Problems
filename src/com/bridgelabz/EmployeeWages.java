03fef6463b0e848a7a25d48a1bff6473ea2a6f80
Krunali <krunalilole29@gmail.com>
2022-03-16 17:26:44 +0530

    UC 2 Employee Daily Wages Added

--- src/com/bridgelabz/EmployeeWages.java
+++ src/com/bridgelabz/EmployeeWages.java
@@ -3,20 +3,26 @@ package com.bridgelabz;
 import java.util.Random;
 public class EmployeeWages
 {
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
 
-        public static void main(String[] args)
+    public static void main(String[] args)
+    {
+        Random random = new Random();
+        int empPresent = random.nextInt(2);
+        System.out.println("Random Value for Employee is :" +empPresent);
+
+        int empWage = 0;
+        if (empPresent == 0)
         {
-
-            Random random = new Random();
-            int empPresent = random.nextInt(2);
-            System.out.println("Random Value for Employee is :" +empPresent);
-            if (empPresent == 0)
-            {
-                System.out.println("Employee is Absent");
-            } else
-            {
-                System.out.println("Employee is Present");
-            }
+            System.out.println("Employee is Absent");
+        } else
+        {
+            empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("Employee is Present");
         }
+        System.out.println("Employee Wage:"+empWage);
+    }
+
 }
 
